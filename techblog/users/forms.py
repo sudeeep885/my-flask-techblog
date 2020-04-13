7eb008ba90e5f130ec22d54b88a926c111d8cb62
sudeep <sudeep885@gmail.com>
2020-04-13 14:45:53 +0530

    constraints in login and update form

--- techblog/users/forms.py
+++ techblog/users/forms.py
@@ -1,13 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, ValidationError
+from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo, Length
 from flask_wtf.file import FileField, FileAllowed
-
-from flask_login import current_user
 from techblog.models import User
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
+    email = StringField('Email', validators=[DataRequired(), Email(message='Not a valid email address.')])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
 
@@ -30,8 +28,8 @@ class RegistrationForm(FlaskForm):
 
 
 class UpdateUserForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[Email(message='Not a valid email address.'), Length(min=6, max=35)])
+    username = StringField('Username', validators=[Length(min=5, max=35)])
     picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
     submit = SubmitField('Update')
 
